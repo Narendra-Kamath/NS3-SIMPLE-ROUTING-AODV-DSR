9fcb82144c0134faa5b20cd284112a356cd2cd83
Narendra Kamath G <narendrakamathg@gmail.com>
2016-04-22 00:37:12 +0530

    Added files via upload

---
+++ naren-aodv.cc
@@ -0,0 +1,125 @@
+#include "ns3/aodv-module.h"
+#include "ns3/core-module.h"
+#include "ns3/network-module.h"
+#include "ns3/internet-module.h"
+#include "ns3/mobility-module.h"
+#include "ns3/wifi-module.h" 
+#include "ns3/applications-module.h"
+#include "ns3/netanim-module.h"
+#include "ns3/flow-monitor-module.h"
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <cmath>
+
+using namespace ns3;
+
+int packetsSent = 0;
+int packetsReceived = 0;
+
+void ReceivePacket (Ptr<Socket> socket)
+{
+  Ptr<Packet> packet;
+  while ((packet = socket->Recv ()))
+    {
+	  packetsReceived++;
+      std::cout<<"Received packet - "<<packetsReceived<<" and Size is "<<packet->GetSize ()<<" Bytes."<<std::endl;
+    }
+}
+
+static void GenerateTraffic (Ptr<Socket> socket, uint32_t pktSize, 
+                             uint32_t pktCount, Time pktInterval )
+{
+  if (pktCount > 0)
+    {
+      socket->Send (Create<Packet> (pktSize));
+      packetsSent++;
+      std::cout<<"Packet sent - "<<packetsSent<<std::endl;
+      
+      Simulator::Schedule (pktInterval, &GenerateTraffic, 
+                           socket, pktSize,pktCount-1, pktInterval);
+    }
+  else
+    {
+      socket->Close ();
+    }
+}
+
+int main(int argc, char **argv)
+{
+  uint32_t size=50;
+  double step=100;
+  double totalTime=100;
+
+  int packetSize = 1024;
+  int totalPackets = totalTime-1;
+  double interval = 1.0; 
+  Time interPacketInterval = Seconds (interval);
+  
+  NodeContainer nodes;
+  NetDeviceContainer devices;
+  Ipv4InterfaceContainer interfaces;
+  
+  std::cout << "Creating " << (unsigned)size << " nodes " << step << " m apart.\n";
+  nodes.Create (size);
+  
+  MobilityHelper mobility;
+  mobility.SetMobilityModel ("ns3::RandomWalk2dMobilityModel",
+                             "Bounds", RectangleValue (Rectangle (-50, 50, -25, 50)));
+  mobility.Install (nodes);
+  
+  NqosWifiMacHelper wifiMac = NqosWifiMacHelper::Default ();
+  wifiMac.SetType ("ns3::AdhocWifiMac");
+  YansWifiPhyHelper wifiPhy = YansWifiPhyHelper::Default ();
+  YansWifiChannelHelper wifiChannel = YansWifiChannelHelper::Default ();
+  wifiPhy.SetChannel (wifiChannel.Create ());
+  WifiHelper wifi = WifiHelper::Default ();
+  wifi.SetRemoteStationManager ("ns3::ConstantRateWifiManager", "DataMode", StringValue ("OfdmRate6Mbps"), "RtsCtsThreshold", UintegerValue (0));
+  devices = wifi.Install (wifiPhy, wifiMac, nodes); 
+  
+  AodvHelper aodv;
+  InternetStackHelper stack;
+  stack.SetRoutingHelper (aodv); 
+  stack.Install (nodes);
+  Ipv4AddressHelper address;
+  address.SetBase ("10.0.0.0", "255.0.0.0");
+  interfaces = address.Assign (devices);
+  
+  Ipv4GlobalRoutingHelper::PopulateRoutingTables ();
+  
+  TypeId tid = TypeId::LookupByName ("ns3::UdpSocketFactory");
+  Ptr<Socket> recvSink = Socket::CreateSocket (nodes.Get (size-1), tid);
+  InetSocketAddress local = InetSocketAddress (Ipv4Address::GetAny (), 8080);
+  recvSink->Bind (local);
+  recvSink->SetRecvCallback (MakeCallback (&ReceivePacket));
+
+  Ptr<Socket> source = Socket::CreateSocket (nodes.Get (0), tid);
+  InetSocketAddress remote = InetSocketAddress (interfaces.GetAddress (size-1,0), 8080);
+  source->Connect (remote);
+  
+  Simulator::Schedule (Seconds (1), &GenerateTraffic, source, packetSize, totalPackets, interPacketInterval);
+                       
+  std::cout << "Starting simulation for " << totalTime << " s ...\n";
+  AnimationInterface anim ("scratch/aodv-output.xml");
+  
+  Ptr<FlowMonitor> flowmon;
+  FlowMonitorHelper flowmonHelper;
+  flowmon = flowmonHelper.InstallAll ();
+  
+	      
+  Simulator::Stop (Seconds (totalTime));
+  Simulator::Run ();
+  flowmon->SetAttribute("DelayBinWidth", DoubleValue(0.01));
+  flowmon->SetAttribute("JitterBinWidth", DoubleValue(0.01));
+  flowmon->SetAttribute("PacketSizeBinWidth", DoubleValue(1));
+  flowmon->CheckForLostPackets();
+  flowmon->SerializeToXmlFile("scratch/aodv-flow.xml", true, true);
+  Simulator::Destroy ();
+  
+  std::cout<<"\n\n***** OUTPUT *****\n\n";
+  std::cout<<"Total Packets sent = "<<packetsSent<<std::endl;
+  std::cout<<"Total Packets received = "<<packetsReceived<<std::endl;
+  std::cout<<"Packet delivery ratio = "<<(float)(packetsReceived/packetsSent)*100<<" %"<<std::endl;
+  
+}
